dfaba35e02034c0f272c4e612bd28564342ddd37
blcoussama <belcadioussama.eng@gmail.com>
2025-06-30 10:55:11 +0100

    Created the Factory fake data in the factory file for tasks and projects + added a tasks FUNCTION in the Project class in the models file + populated the user and project factories in the DatabaseSeeder.php FILE

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -17,7 +17,13 @@ class ProjectFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->sentence(),
+            'description' => fake()->realText(),
+            'due_date' => fake()->dateTimeBetween('now', '+1 year'),
+            'status' => fake()->randomElement(['pending', 'in_progress', 'completed']),
+            'image_path' => fake()->imageUrl(),
+            'created_by' => 1, // Assuming user with ID 1 exists
+            'updated_by' => 1, // Assuming user with ID 1 exists
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Project;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,8 +18,12 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Oussama',
+            'email' => 'Oussama@example.com',
+            'password' => bcrypt('password123'), // Ensure to set a password
+            'email_verified_at' => time()
         ]);
+
+        Project::factory()->count(30)->hasTasks(30)->create();
     }
 }
