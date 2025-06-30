fff0d43bd7f679792c383f2a9a95747a0aa2c32b
blcoussama <belcadioussama.eng@gmail.com>
2025-06-30 10:42:04 +0100

    Created the Columns for the project and task in their migrations files

--- database/migrations/2025_06_30_093500_create_tasks_table.php
+++ database/migrations/2025_06_30_093500_create_tasks_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,16 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->string('image_path')->nullable();
+            $table->string('status');
+            $table->string('priority');
+            $table->timestamp('due_date')->nullable();
+            $table->foreignId('assigned_user_id')->constrained('users');
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('updated_by')->constrained('users');
+            $table->foreignId('project_id')->constrained('projects');
             $table->timestamps();
         });
     }
